b2baae99d082c1bbb63c6b5bbd3fb5d12cd189cf
Amila Abeygunasekara <auabey@gmail.com>
2024-02-03 22:21:39 -0500

    Create a VS project for Lighting chapter and move external dependencies to the root

---
+++ Lighting/Lighting.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
