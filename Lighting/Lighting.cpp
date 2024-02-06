a8adcb01b19c97018a53f896a80f84e20fd03158
Amila Abeygunasekara <auabey@gmail.com>
2024-02-06 01:23:30 -0500

    Colors - Make the background pitch black

--- Lighting/Lighting.cpp
+++ Lighting/Lighting.cpp
@@ -209,7 +209,7 @@ void initOpengl()
 
 void renderLoop()
 {
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     containerShader->use();
