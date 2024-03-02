4d090559ec26f3d9c52f20d67cdfdebdde436890
Amila Abeygunasekara <auabey@gmail.com>
2024-03-02 01:46:32 -0500

    Materials - Alternating colors

--- Lighting/Lighting.cpp
+++ Lighting/Lighting.cpp
@@ -228,9 +228,22 @@ void renderLoop()
     containerShader->setVec3("light.specular", glm::vec3(1.0f));
 
     // Gold material
-    containerShader->setVec3("material.ambient", glm::vec3(0.24725f, 0.1995f, 0.0745f));
-    containerShader->setVec3("material.diffuse", glm::vec3(0.75164f, 0.60648f, 0.22648f));
-    containerShader->setVec3("material.specular", glm::vec3(0.628281f, 0.555802f, 0.366065f));
+    //containerShader->setVec3("material.ambient", glm::vec3(0.24725f, 0.1995f, 0.0745f));
+    //containerShader->setVec3("material.diffuse", glm::vec3(0.75164f, 0.60648f, 0.22648f));
+    //containerShader->setVec3("material.specular", glm::vec3(0.628281f, 0.555802f, 0.366065f));
+    //containerShader->setFloat("material.shininess", 16.0f);
+
+    glm::vec3 cubeLightColor;
+    cubeLightColor.x = sin((float)glfwGetTime() * 2.0f);
+    cubeLightColor.y = sin((float)glfwGetTime() * 0.7f);
+    cubeLightColor.z = sin((float)glfwGetTime() * 1.3f);
+
+    glm::vec3 diffuseColor = cubeLightColor * glm::vec3(0.8f);
+    glm::vec3 ambientColor = diffuseColor * glm::vec3(0.2f);
+
+    containerShader->setVec3("material.ambient", ambientColor);
+    containerShader->setVec3("material.diffuse", diffuseColor);
+    containerShader->setVec3("material.specular", glm::vec3(0.5f));
     containerShader->setFloat("material.shininess", 16.0f);
 
     glm::mat4 view = glm::lookAt(cameraPosition, cameraPosition + cameraFront, world_up);
